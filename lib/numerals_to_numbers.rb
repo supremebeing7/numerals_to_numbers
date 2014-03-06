a3c8c8aa6d3b9e1ca2cb6dd27d1abc01161abe9c
Mark Lehman <markopolo@gmail.com>
2014-03-05 17:14:00 -0800

    refactored to remove hard-coded exceptions like "4, 9, 40, 90" etc but still fully functional

--- lib/numerals_to_numbers.rb
+++ lib/numerals_to_numbers.rb
@@ -1,55 +1,26 @@
-def numerals_to_numbers(numerals)
-  numerals.upcase!
+def numerals_to_numbers(numeral)
+  numeral.upcase!
+  numbers_array = numeral.split('')
 
   numeral_pairs = {
-    /I/ => '1.',
-    /V/ => '5.',
-    /IV/ => '4.', 
-    /X/ => '10.',
-    /IX/ => '9.',
-    /L/ => '50.',
-    /XL/ => '40.',
-    /C/ => '100.',
-    /XC/ => '90.',
-    /D/ => '500.',
-    /CD/ => '400.',
-    /M/  => '1000.',
-    /CM/ => '900.',
- }
-    
-
-  numeral_pairs.reverse_each do |key, value|
-    numerals.gsub!(key, value)
-    puts numerals  
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M'  => 1000
+  }
+  
+  result = 0  
+  numbers_array.each_with_index do |letter, index|
+    result += numeral_pairs[letter] 
+    if numbers_array[index + 1] != nil && numeral_pairs[ numbers_array[index] ] < numeral_pairs[ numbers_array[index + 1] ] 
+      result -= numeral_pairs[ numbers_array[index] ] * 2
+    end
   end
-
-  numbers_as_strings = numerals.split('.')
-  numbers_as_integers = []
-  numbers_as_strings.each do |number_string|
-    numbers_as_integers << number_string.to_i
-  end
-
-  numbers_as_integers.inject(:+)
+  result
 end
 
-puts numerals_to_numbers("mcmlxXXiv")
-
-# numerals.gsub!(/CM/, '900.')
-  # numerals.gsub!(/M/, '1000.')
-  
-  # numerals.gsub!(/CD/, '400.')
-  # numerals.gsub!(/D/, '500.')
-  
-  # numerals.gsub!(/XC/, '90.')
-  # numerals.gsub!(/C/, '100.')
-  
-  # numerals.gsub!(/XL/, '40.')
-  # numerals.gsub!(/L/, '50.')
-  
-  # numerals.gsub!(/IX/, '9.')
-  # numerals.gsub!(/X/, '10.')
-  
-  # numerals.gsub!(/IV/, '4.')
-  # numerals.gsub!(/V/, '5.')
-  # numerals.gsub!(/I/, '1.')
+puts numerals_to_numbers("mcmlxxxiv")
 
